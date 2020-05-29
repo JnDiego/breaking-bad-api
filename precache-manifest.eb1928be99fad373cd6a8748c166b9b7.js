37ed856d4dda2dc9d5289d654b1fbb85a79e48e5
Juan Diego <diego_suarez@outlook.com>
2020-05-29 13:28:52 -0500

    Updates

---
+++ precache-manifest.eb1928be99fad373cd6a8748c166b9b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c4b9b6a3212e57d8b55d3023b82dc19",
+    "url": "/breaking-bad-api/index.html"
+  },
+  {
+    "revision": "ea73e8dd57197de561d2",
+    "url": "/breaking-bad-api/static/css/main.1181666f.chunk.css"
+  },
+  {
+    "revision": "3a720da1c3fd383bca22",
+    "url": "/breaking-bad-api/static/js/2.c3786d44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breaking-bad-api/static/js/2.c3786d44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea73e8dd57197de561d2",
+    "url": "/breaking-bad-api/static/js/main.1236cb21.chunk.js"
+  },
+  {
+    "revision": "281c0cca9c8cc3f7e29c",
+    "url": "/breaking-bad-api/static/js/runtime-main.605541b3.js"
+  },
+  {
+    "revision": "427a4adc3c0a0dec192d14617aaa6c4c",
+    "url": "/breaking-bad-api/static/media/logo.427a4adc.svg"
+  }
+]);
